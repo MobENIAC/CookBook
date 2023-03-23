e88556d806e3b2148ff03ca6e6f40d0ef325cb5e
Tas <tasqur9@gmail.com>
2023-03-23 10:48:46 +0100

    change path

---
+++ CookBook.Api/Migrations/20230323094616_AddAllModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CookBook.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAllModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
