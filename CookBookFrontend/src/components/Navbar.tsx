b6b1c55bcb09600f07d8c309002ade13b698ca4a
Salt Dev <saltdev@salt-devs-MacBook-Pro.local>
2023-03-24 16:58:22 +0100

    frontend - change css path for navbar

--- CookBookFrontend/src/components/Navbar.tsx
+++ CookBookFrontend/src/components/Navbar.tsx
@@ -1,14 +1,11 @@
 import { Link } from "react-router-dom";
-import "../styleSheets/NavbarSS.css";
+import "../stylesheets/NavbarSS.css";
 
 export const Navbar = () => {
     return ( 
         <div className="topnav">
             <Link className="navbar__links" to="/">Home</Link>
-            <Link className="navbar__links" to="/about">About</Link>
-        
-          {/* <a className="active" href="#home">Home</a> */}
-         
+            <Link className="navbar__links" to="/about">About</Link>         
         </div> 
         );
 };
