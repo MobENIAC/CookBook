baeba393e3e4446db8dbc66051cc4fc6a1a1dbff
Salt Dev <saltdev@salt-devs-MacBook-Pro.local>
2023-03-27 09:28:39 +0200

    merge conflits cleared

--- CookBookFrontend/src/components/Navbar.tsx
+++ CookBookFrontend/src/components/Navbar.tsx
@@ -2,17 +2,6 @@ import { Link } from "react-router-dom";
 import "../stylesheets/NavbarSS.css";
 
 export const Navbar = () => {
-<<<<<<< HEAD
-    return ( 
-        <div className="topnav">
-            <Link className="navbar__links" to="/">Home</Link>
-            <Link className="navbar__links" to="/about">About</Link>         
-        </div> 
-        );
-};
-
-
-=======
   return (
       // <h1>Cookbook</h1>
     <div className="topnav">
@@ -25,7 +14,6 @@ export const Navbar = () => {
       <Link className="navbar__links" to="/add">
         Add Recipes
       </Link>
->>>>>>> Development2
 
       {/* <a className="active" href="#home">Home</a> */}
     </div>
