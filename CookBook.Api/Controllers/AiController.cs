0e5c40bcfc74d14d1ceff2b898ff8120a8c4e1f9
Tas <tasqur9@gmail.com>
2023-03-29 17:14:14 +0200

    try to implement azure key vault

--- CookBook.Api/Controllers/AiController.cs
+++ CookBook.Api/Controllers/AiController.cs
@@ -46,7 +46,6 @@ public class AiController : ControllerBase
         }
 
         return Ok(outputResult);
-
     }
 
 }
--- CookBook.Api/Program.cs
+++ CookBook.Api/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using CookBook.Api.Services;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("CookBookConnection") ?? throw new InvalidOperationException("Connection string 'CookBookConnection' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CookBookConnection") ?? throw new InvalidOperationException("Connection string 'CookBookConnection' not found."))
+    );
 builder.Services.AddScoped<IIngridientsClient, IngridientsClient>();
 // Add services to the container.
 
@@ -13,6 +18,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// string value = System.Configuration.ConfigurationManager.AppSettings[key];
+
 builder.Configuration.AddUserSecrets<Program>();
 var chatGptApiKey = builder.Configuration["chatGptApiKey:apiKey1:"];
 
@@ -31,10 +39,30 @@ app.UseCors(policy =>
               .AllowAnyHeader();  //set the allowed origin
   });
 
+SecretClientOptions options = new SecretClientOptions()
+{
+    Retry =
+        {
+            Delay= TimeSpan.FromSeconds(2),
+            MaxDelay = TimeSpan.FromSeconds(16),
+            MaxRetries = 5,
+            Mode = RetryMode.Exponential
+         }
+};
+var client = new SecretClient(new Uri("https://cookbookkeys.vault.azure.net/"), new DefaultAzureCredential(), options);
+
+KeyVaultSecret secret = client.GetSecret("testname");
+
+string secretValue = secret.Value;
+
+builder.Configuration.AddAzureKeyVault(
+ new Uri("https://cookbookkeys.vault.azure.net/"), new DefaultAzureCredential()
+);
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+/* app.MapGet("/", () => secretValue); */
 app.MapControllers();
 
 app.Run();
